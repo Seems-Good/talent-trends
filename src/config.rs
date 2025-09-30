a2e80ef88a31397e0915071305f9a66654aa7087
Jeremy-Gstein <jeremy51b5@pm.me>
2025-09-30 05:24:30 -0400

    v0.0.4 - remove dead code.

--- src/config.rs
+++ src/config.rs
@@ -30,19 +30,19 @@ impl ClassSpecs {
         toml::from_str(CONFIG).expect("Failed to parse classes.toml")
     }
     
-    pub fn class_names(&self) -> Vec<String> {
-        self.classes.keys()
-            .map(|k| k.replace('_', " "))
-            .collect()
-    }
-    
-    pub fn get_specs(&self, class_name: &str) -> Option<&Vec<String>> {
-        let key = class_name.replace(' ', "_");
-        self.classes.get(&key).map(|c| &c.specs)
-    }
+    // pub fn class_names(&self) -> Vec<String> {
+    //     self.classes.keys()
+    //         .map(|k| k.replace('_', " "))
+    //         .collect()
+    // }
+    //
+    // pub fn get_specs(&self, class_name: &str) -> Option<&Vec<String>> {
+    //     let key = class_name.replace(' ', "_");
+    //     self.classes.get(&key).map(|c| &c.specs)
+    // }
 }
 
-// Current tier encounters (Season 1 of The War Within)
+// (Season 3 of The War Within)
 pub fn get_encounters() -> Vec<Encounter> {
     vec![
          Encounter { id: 3129, name: "Plexus Sentinel" },
@@ -68,59 +68,7 @@ pub fn get_regions() -> Vec<Region> {
 }
 
 
-
-// use serde::Deserialize;
-// use std::collections::BTreeMap;
-//
-// #[derive(Debug, Deserialize)]
-// pub struct ClassSpecs {
-//     #[serde(flatten)]
-//     pub classes: BTreeMap<String, ClassData>,
-// }
-//
-// #[derive(Debug, Deserialize)]
-// pub struct ClassData {
-//     pub specs: Vec<String>,
-// }
-//
-// #[derive(Debug, Clone)]
-// pub struct Encounter {
-//     pub id: i32,
-//     pub name: &'static str,
-// }
-//
-// impl ClassSpecs {
-//     pub fn load() -> Self {
-//         const CONFIG: &str = include_str!("../classes.toml");
-//         toml::from_str(CONFIG).expect("Failed to parse classes.toml")
-//     }
-//
-//     pub fn class_names(&self) -> Vec<String> {
-//         self.classes.keys()
-//             .map(|k| k.replace('_', " "))
-//             .collect()
-//     }
-//
-//     pub fn get_specs(&self, class_name: &str) -> Option<&Vec<String>> {
-//         let key = class_name.replace(' ', "_");
-//         self.classes.get(&key).map(|c| &c.specs)
-//     }
-// }
-//
-// // Current tier encounters (Season 1 of The War Within)
-// pub fn get_encounters() -> Vec<Encounter> {
-//     vec![
-//         Encounter { id: 3129, name: "Plexus Sentinel" },
-//         Encounter { id: 3131, name: "Loom'ithar" },
-//         Encounter { id: 3130, name: "Soulbinder Naazindhri" },
-//         Encounter { id: 3132, name: "Forgeweaver Araz" },
-//         Encounter { id: 3122, name: "The Soul Hunters" },
-//         Encounter { id: 3133, name: "Fractillus" },
-//         Encounter { id: 3134, name: "Nexus-King Salahadaar" },
-//         Encounter { id: 3135, name: "Dimensius, the All-Devouring" },
-//     ]
-//
-//
+// (Season 1 The War Within)
 //     // vec![
 //     //     Encounter { id: 2902, name: "Ulgrax the Devourer" },
 //     //     Encounter { id: 2917, name: "The Bloodbound Horror" },
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use axum::{
     extract::Query,
-    response::{Html, sse::{Event, Sse}, Response},
+    response::{Html, sse::{Event, Sse}},
     routing::get,
     Router,
 };
-use futures::stream::{self, Stream};
-use serde::{Deserialize, Serialize};
+use futures::stream::Stream;
+use serde::Deserialize;
 use std::{convert::Infallible, net::SocketAddr, time::Duration};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-use tokio_stream::StreamExt as _;
 
 mod config;
 mod warcraftlogs;
@@ -114,96 +113,3 @@ async fn get_talents_sse(
             .text("keep-alive")
     )
 }
-
-
-
-// use axum::{
-//     extract::Query,
-//     response::Html,
-//     routing::get,
-//     Router,
-// };
-// use serde::{Deserialize, Serialize};
-// use std::net::SocketAddr;
-// use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-//
-// mod config;
-// mod warcraftlogs;
-// mod templates;
-//
-// use config::ClassSpecs;
-//
-// #[tokio::main]
-// async fn main() -> anyhow::Result<()> {
-//     // Load .env file
-//     dotenvy::dotenv().ok();
-//
-//     tracing_subscriber::registry()
-//         .with(
-//             tracing_subscriber::fmt::layer()
-//                 .with_target(false)
-//                 .compact()
-//         )
-//         .init();
-//
-//     let config = ClassSpecs::load();
-//     tracing::info!("Loaded {} classes", config.classes.len());
-//
-//     let app = Router::new()
-//         .route("/", get(home))
-//         .route("/api/talents", get(get_talents));
-//
-//     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
-//     tracing::info!("Server listening on http://{}", addr);
-//
-//     let listener = tokio::net::TcpListener::bind(addr).await?;
-//     axum::serve(listener, app).await?;
-//
-//     Ok(())
-// }
-//
-// async fn home() -> Html<String> {
-//     let config = ClassSpecs::load();
-//     Html(templates::home(&config))
-// }
-//
-// #[derive(Deserialize)]
-// struct TalentQuery {
-//     class: String,
-//     spec: String,
-//     encounter: i32,
-//     region: String,
-// }
-//
-// async fn get_talents(Query(params): Query<TalentQuery>) -> Html<String> {
-//     let region_display = if params.region == "all" {
-//         "All Regions"
-//     } else {
-//         &params.region
-//     };
-//
-//     tracing::info!(
-//         "Fetching talents for {} {} on encounter {} in {} region.",
-//         params.class,
-//         params.spec,
-//         params.encounter,
-//         region_display
-//     );
-//
-//     let region = if params.region == "all" {
-//         None
-//     } else {
-//         Some(params.region.as_str())
-//     };
-//
-//
-//     match warcraftlogs::fetch_top_talents(&params.class, &params.spec, params.encounter, region).await {
-//         Ok(data) => Html(templates::render_talents(&data)),
-//         Err(e) => {
-//             tracing::error!("Failed to fetch talents: {:#}", e);
-//             Html(format!(r#"<div class="error">Error: {}</div>"#, e))
-//         }
-//     }
-// }
-//
-
